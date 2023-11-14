d54248bfb317898f6d54606df308e6cb24f598d9
Kofi <kufuorkofi@gmail.com>
2023-11-14 11:29:04 +0000

    commit

--- models/erc4337/labels/erc4337_labels_factories.sql
+++ models/erc4337/labels/erc4337_labels_factories.sql
@@ -6,7 +6,8 @@
 
 SELECT name, address
 FROM (VALUES
-('biconomy', '0x000000f9ee1842bb72f6bbdd75e6d3d4e3e9594c'),    
+('biconomy', '0x000000f9ee1842bb72f6bbdd75e6d3d4e3e9594c'), 
+('biconomy', '0x000000a56aaca3e9a4c479ea6b6cd0dbcb6634f5'),    
 ('safe', '0xa6b71e26c5e0845f74c812102ca7114b6a896ab2'),    
 ('zerodev_kernel', '0xaee9762ce625e0a8f7b184670fb57c37bfe1d0f1'),    
 ('zerodev_kernel', '0x4e4946298614fc299b50c947289f4ad0572cb9ce'),    
