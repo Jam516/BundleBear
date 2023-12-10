d13a7dde791e090cfceef518a444d1898130fc88
Kofi <kufuorkofi@gmail.com>
2023-12-10 13:26:05 +0000

    commit

--- models/erc4337/labels/erc4337_labels_factories.sql
+++ models/erc4337/labels/erc4337_labels_factories.sql
@@ -19,6 +19,7 @@ FROM (VALUES
 ('fun.xyz', '0xda2ba73e0dd91d8bbb72dc10655b691295bdf985'),
 ('candide', '0xb73eb505abc30d0e7e15b73a492863235b3f4309'),
 ('simpleaccount', '0x15ba39375ee2ab563e8873c8390be6f2e2f50232'),
+('simpleaccount', '0x9406cc6185a346906296840746125a0e44976454'),
 ('alchemy_lightaccount', '0x000000893a26168158fbeadd9335be5bc96592e2'),
 ('etherspot', '0x7f6d8f107fe8551160bd5351d5f1514a6ad5d40e')
 ) AS x (name, address)
