b74dcb729c8150b2908f499275a0534ecf284b7c
Kofi <kufuorkofi@gmail.com>
2024-02-28 17:02:18 +0000

    commit

---
+++ models/smart_accounts/tvl/ethereum/smart_accounts_starting_balance_erc4337.sql
@@ -0,0 +1,25 @@
+{{ config
+(
+    materialized = 'table'
+)
+}}
+
+with
+address_table as (  
+    select ACCOUNT_ADDRESS as address_filter
+    from BUNDLEBEAR.DBT_KOFI.ERC4337_ETHEREUM_ACCOUNT_DEPLOYMENTS
+)
+
+select
+    '2023-01-01 00:00:00.000'::timestamp as date,
+    balances.address,
+    balances.usd_balance,
+    balances.balance,
+    balances.token_address,
+    balances.token_symbol
+from ETHEREUM.ASSETS.ETH_AND_ERC20_BALANCES_LATEST balances 
+inner join address_table 
+    on address = address_filter
+    AND block_timestamp <= TO_DATE('2023-04-01', 'YYYY-MM-DD')
+    and balances.usd_balance is not null
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY balances.address, balances.token_address ORDER BY balances.block_timestamp DESC) = 1
