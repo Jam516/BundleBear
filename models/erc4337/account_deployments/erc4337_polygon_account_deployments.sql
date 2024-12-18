e838ee4f1cdd4dec605198b02249419455bce301
Kofi <kufuorkofi@gmail.com>
2024-12-18 06:35:58 +0000

    commit

--- models/erc4337/account_deployments/erc4337_polygon_account_deployments.sql
+++ models/erc4337/account_deployments/erc4337_polygon_account_deployments.sql
@@ -26,7 +26,7 @@ INNER JOIN {{ source('polygon_raw', 'transactions') }} t
     ('0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789', 
     '0x0000000071727de22e5e9d8baf0edac6f37da032',
     '0x0576a174d229e3cfa37253523e645a78a0c91b57', 
-    '0x0f46c65c17aa6b4102046935f33301f0510b163a',)
+    '0x0f46c65c17aa6b4102046935f33301f0510b163a')
     {% if is_incremental() %}
     AND l.BLOCK_TIMESTAMP >= CURRENT_TIMESTAMP() - interval '3 day' 
     AND t.BLOCK_TIMESTAMP >= CURRENT_TIMESTAMP() - interval '3 day' 
