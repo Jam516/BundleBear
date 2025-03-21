e20af7f1eec9db0f4e742ade077c0ec0a82bb74f
Will Minshew <wminshew@gmail.com>
2025-03-20 20:19:47 -0700

    Update erc4337_labels_factories.sql

--- models/erc4337/labels/erc4337_labels_factories.sql
+++ models/erc4337/labels/erc4337_labels_factories.sql
@@ -45,5 +45,6 @@ FROM (VALUES
 ('zerodev_kernel', '0xd703aae79538628d27099b8c4f621be4ccd142d5'),
 ('coinbase_smart_wallet', '0x0ba5ed0c6aa8c49038f819e587e2633c4a9f428a'),
 ('lumx', '0x2e1c14daadefc4a85eaec81dacba27cd455a0b66'),
-('Send', '0x008c9561857b6555584d20ac55110335759aa2c2')
+('Send', '0x008c9561857b6555584d20ac55110335759aa2c2'),
+('splits_smart_vault', '0x8E6Af8Ed94E87B4402D0272C5D6b0D47F0483e7C')
 ) AS x (name, address)
