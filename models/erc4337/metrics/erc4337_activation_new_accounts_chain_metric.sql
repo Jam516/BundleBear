15b1735d7faa0f855ae41f205aada08f3db2e709
Jam516 <kufuorkofi@gmail.com>
2026-04-09 09:08:52 +0100

    optmise tables

--- models/erc4337/metrics/erc4337_activation_new_accounts_chain_metric.sql
+++ models/erc4337/metrics/erc4337_activation_new_accounts_chain_metric.sql
@@ -1,7 +1,8 @@
 {{ config
 (
-    materialized = 'table',
-    copy_grants=true
+    materialized = 'incremental',
+    copy_grants=true,
+    unique_key = ['date', 'timeframe', 'chain']
 )
 }} 
 
@@ -10,9 +11,13 @@ WITH day_metrics AS (
     TO_VARCHAR(DATE, 'YYYY-MM-DD') as DATE,
     CHAIN,
     'day' AS TIMEFRAME,
-    COUNT(SENDER) AS NUM_ACCOUNTS,
+    COUNT(SENDER) AS NUM_ACCOUNTS
     FROM BUNDLEBEAR.DBT_KOFI.ERC4337_ALL_ACCOUNT_ACTIVATIONS
+    {% if is_incremental() %}
+    WHERE DATE >= DATE_TRUNC('day', CURRENT_DATE()) - INTERVAL '3 day'
+    {% else %}
     WHERE DATE > DATE_TRUNC('month', CURRENT_DATE()) - INTERVAL '12 months'
+    {% endif %}
     GROUP BY 
     1, 2, 3
 )
@@ -22,9 +27,13 @@ WITH day_metrics AS (
     TO_VARCHAR(DATE_TRUNC('week', DATE), 'YYYY-MM-DD') as DATE,
     CHAIN,
     'week' AS TIMEFRAME,
-    COUNT(SENDER) AS NUM_ACCOUNTS,
+    COUNT(SENDER) AS NUM_ACCOUNTS
     FROM BUNDLEBEAR.DBT_KOFI.ERC4337_ALL_ACCOUNT_ACTIVATIONS
+    {% if is_incremental() %}
+    WHERE DATE >= DATE_TRUNC('week', CURRENT_DATE()) - INTERVAL '3 week'
+    {% else %}
     WHERE DATE > DATE_TRUNC('month', CURRENT_DATE()) - INTERVAL '12 months'
+    {% endif %}
     GROUP BY 
     1, 2, 3
 )
@@ -34,9 +43,13 @@ WITH day_metrics AS (
     TO_VARCHAR(DATE_TRUNC('month', DATE), 'YYYY-MM-DD') as DATE,
     CHAIN,
     'month' AS TIMEFRAME,
-    COUNT(SENDER) AS NUM_ACCOUNTS,
+    COUNT(SENDER) AS NUM_ACCOUNTS
     FROM BUNDLEBEAR.DBT_KOFI.ERC4337_ALL_ACCOUNT_ACTIVATIONS
+    {% if is_incremental() %}
+    WHERE DATE >= DATE_TRUNC('month', CURRENT_DATE()) - INTERVAL '3 month'
+    {% else %}
     WHERE DATE > DATE_TRUNC('month', CURRENT_DATE()) - INTERVAL '12 months'
+    {% endif %}
     GROUP BY 
     1, 2, 3
 )
